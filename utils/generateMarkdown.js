1c3490539c4ec367e2a7b5c07aff4b5d1d33e376
Thannia Hernandez <thaherna1@gmail.com>
2024-04-28 21:26:16 -0500

    added license function in generateMardown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license){
+   switch (license) {
+    case 'MIT License':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]';
+    case 'Apache License 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+   case 'Mozilla Public License 2.0':
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+   default:
+    return '';
+    } 
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license) {
+    switch (license) {
+      case 'MIT License':
+        return '[MIT License](https://opensource.org/licenses/MIT)';
+      case 'Apache License 2.0':
+        return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
+      case 'Mozilla Public Lincense 2.0':
+        return '[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)';
+      default:
+        return '';
+    }
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License 
+    
+    This project is licensed under the ${renderLicenseLink(license)} license.`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
 }
 
 module.exports = generateMarkdown;
