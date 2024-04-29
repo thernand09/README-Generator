d45d162b9602a5958bde78f2c0953a23110a1e0b
Thannia Hernandez <thaherna1@gmail.com>
2024-04-28 20:27:22 -0500

    added README.md file to repo

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+//Requires the generateMarkdown in file
+const generateMarkdown = require('./utils/generateMarkdown');
+//Calls file system
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a brief description of your project:',   
+    },
+    {
+        type: 'checkbox',
+        name: 'contents',
+        message: '',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
